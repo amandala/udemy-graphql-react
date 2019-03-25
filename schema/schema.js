1b47716f33d51523bb196a39eb0d3a8cd8905de7
Amanda <amanda.haynes@versett.com>
2019-03-25 12:55:21 -0600

    unidirectional and bidirectional relations

--- schema/schema.js
+++ schema/schema.js
@@ -1,29 +1,63 @@
 const axios = require("axios");
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-
-const UserType = new GraphQLObjectType({
-  name: "User",
-  fields: {
-    id: { type: GraphQLString },
-    firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
-  }
-});
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList
+} = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
+  fields: () => ({
+    // the arrow function here delays execution of this fields type until after
+    // the whiole file has been evaluated to deal with circular referenced and the
+    //fact that UserType is defined after this type
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(resp => resp.data);
+      }
+    }
+  })
+});
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(resp => resp.data);
+      }
+    }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then(resp => resp.data);
+      }
+    },
     user: {
       type: UserType,
       args: { id: { type: GraphQLString } },
@@ -32,7 +66,7 @@ const RootQuery = new GraphQLObjectType({
           axios
             .get(`http://localhost:3000/users/${args.id}`)
             // necesary to make GQL work with the axios response nesting the
-            // data in the data object
+            // data in the data object. can be abstracted to a helper
             .then(resp => resp.data)
         );
       }
